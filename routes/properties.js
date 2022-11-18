75475e2674153df4f583b706bbcfe37c8e856862
Ayush Goel <ayushgoel95@gmail.com>
2022-11-18 13:35:56 -0600

    Initial Commit
    CRUD for reservations and properties implemented

---
+++ routes/properties.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+
+var monk = require('monk');
+var db = monk('localhost:27017/milesNstay');
+
+/* GET listings according available */
+router.get('/', function(req, res) {
+  var collection = db.get('properties');
+  collection.find({}, function(err, properties){
+      if(err) throw err;
+      res.json(properties);
+  });
+});
+
+/* GET listings according to id. */
+router.get('/:id', function(req, res) {
+  var collection = db.get('properties');
+  collection.findOne({"_id": req.params.id}, function(err, properties){
+      if(err) throw err;
+      res.json(properties);
+  });
+});
+
+/* ADD a new property listing to the database */
+router.post('/', function(req, res) {
+  var collection = db.get('properties');
+  collection.insert(req.body.data, function(err, property) {
+    if(err) throw err;
+    res.json({'property_id': property._id});
+  });
+});
+
+/* EDIT/UPDATE an existing property listing to the database */
+router.put('/', function(req, res) {
+  var collection = db.get('properties');
+  collection.update(req.body.query, {"$set": req.body.newValue}, function(err, result) {
+    if(err) throw err;
+    res.json(result);
+  });
+});
+
+/* DELETE an existing property listing from the database */
+router.delete('/', function(req, res) {
+  var collection = db.get('properties');
+  collection.remove(req.body.query, function(err, result) {
+    if(err) throw err;
+    res.json(result);
+  });
+});
+
+module.exports = router;
---
+++ routes/reservations.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+
+var monk = require('monk');
+var db = monk('localhost:27017/milesNstay');
+
+/* GET reservations according to guest id. */
+router.get('/:id', function(req, res) {
+  var collection = db.get('reservations');
+  collection.find({"guest.guest_id": req.params.id}, function(err, reservations){
+      if(err) throw err;
+      res.json(reservations);
+  });
+});
+
+/* ADD reservations to the database */
+router.post('/', function(req, res) {
+  var collection = db.get('reservations');
+  collection.insert(req.body.data, function(err, reservation) {
+    if(err) throw err;
+    res.json({"reservation_id": reservation._id});
+  });
+});
+
+/* UPDATE/EDIT an existing reservation */
+router.put('/', function(req, res) {
+  var collection = db.get('reservations');
+  collection.update(req.body.query, {"$set": req.body.newValue}, function(err, result) {
+    if(err) throw err;
+    res.json(result);
+  });
+});
+
+/* DELETE an existing reservation from the database */
+router.delete('/', function(req, res) {
+  var collection = db.get('reservations');
+  collection.remove(req.body.query, function(err, result) {
+    if(err) throw err;
+    res.json(result);
+  });
+});
+
+module.exports = router;
